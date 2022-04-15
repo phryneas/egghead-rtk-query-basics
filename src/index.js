070336daca6f14a2fe130e14af68d1a3ebfe9c0d
Lenz Weber <mail@lenzw.de>
2022-04-15 14:11:57 +0200

    Fetch Data from a Live Endpoint with RTK Query

--- src/index.js
+++ src/index.js
@@ -2,62 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createApi, ApiProvider } from "@reduxjs/toolkit/query/react";
 
-// data copy & pasted from https://pokeapi.co/api/v2/pokemon?limit=9
-const fakePokemonListing = {
-  count: 1126,
-  next: "https://pokeapi.co/api/v2/pokemon?offset=9&limit=9",
-  previous: null,
-  results: [
-    { name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/" },
-    { name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon/2/" },
-    { name: "venusaur", url: "https://pokeapi.co/api/v2/pokemon/3/" },
-    { name: "charmander", url: "https://pokeapi.co/api/v2/pokemon/4/" },
-    { name: "charmeleon", url: "https://pokeapi.co/api/v2/pokemon/5/" },
-    { name: "charizard", url: "https://pokeapi.co/api/v2/pokemon/6/" },
-    { name: "squirtle", url: "https://pokeapi.co/api/v2/pokemon/7/" },
-    { name: "wartortle", url: "https://pokeapi.co/api/v2/pokemon/8/" },
-    { name: "blastoise", url: "https://pokeapi.co/api/v2/pokemon/9/" },
-  ],
-};
-// partial data of https://pokeapi.co/api/v2/pokemon/1/
-const fakePokemonDetailData = {
-  id: 1,
-  name: "bulbasaur",
-  height: 7,
-  weight: 69,
-  types: [
-    {
-      slot: 1,
-      type: { name: "grass", url: "https://pokeapi.co/api/v2/type/12/" },
-    },
-    {
-      slot: 2,
-      type: { name: "poison", url: "https://pokeapi.co/api/v2/type/4/" },
-    },
-  ],
-  sprites: {
-    front_default:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-  },
-};
-
-function simulateLoading() {
-  return new Promise((resolve) => setTimeout(resolve, 500));
-}
-
 const api = createApi({
   baseQuery: () => {},
   endpoints: (build) => ({
     pokemonList: build.query({
       async queryFn() {
-        await simulateLoading();
-        return { data: fakePokemonListing };
+        const result = await fetch("https://pokeapi.co/api/v2/pokemon?limit=9");
+        if (result.ok) {
+          const data = await result.json();
+          return { data };
+        } else {
+          return { error: "something went wrong" };
+        }
       },
     }),
     pokemonDetail: build.query({
       async queryFn() {
-        await simulateLoading();
-        return { data: fakePokemonDetailData };
+        const result = await fetch("https://pokeapi.co/api/v2/pokemon/1/");
+        if (result.ok) {
+          const data = await result.json();
+          return { data };
+        } else {
+          return { error: "something went wrong" };
+        }
       },
     }),
   }),
